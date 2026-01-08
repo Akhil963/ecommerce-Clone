b8557371e4b182a43d9fd8aff9094127aa25791c
Akhilesh Bhandakkar <Akhil963@users.noreply.github.com>
2026-01-09 01:59:37 +0530

    Add re

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Only for development - comment out in production
+if (process.env.NODE_ENV === 'development') {
+  console.warn = () => {};
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
